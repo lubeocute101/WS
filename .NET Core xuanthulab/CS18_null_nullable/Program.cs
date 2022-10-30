cc69cd03bf883dc9edaef8628facef5caf5062bf
Lucute2001 <lch.ryu2001@gmail.com>
2022-10-31 01:12:21 +0700

    CS18_null_nullable

---
+++ .NET Core xuanthulab/CS18_null_nullable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace CS18_null_nullable
+{
+    class ABC
+    {
+        public void XinChao() => System.Console.WriteLine("Xin chao C#");
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.Clear();
+            int? age;
+            age = null;
+            age = 100;
+            if (age.HasValue)
+            {
+                int _age = age.Value;
+                System.Console.WriteLine(_age);
+            }
+            else{
+                System.Console.WriteLine("NULL");
+            }
+        }
+    }
+}
