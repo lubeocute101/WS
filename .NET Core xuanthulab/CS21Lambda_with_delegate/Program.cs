01b588aa5ad9ab98c821caebe5ee792f4b7899ad
Lucute2001 <lch.ryu2001@gmail.com>
2022-11-01 17:03:29 +0700

    CS21 lambda

--- .NET Core xuanthulab/CS21Lambda_with_delegate/Program.cs
+++ .NET Core xuanthulab/CS21Lambda_with_delegate/Program.cs
@@ -10,21 +10,58 @@ namespace CS21Lambda_with_delegate
         1)
         (int a, int b) => 
         */
+        static void Test(int a, int b, Func<int, int, int> callback)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            System
+                .Console
+                .WriteLine($"a + b = {a} + {b} = {callback?.Invoke(a, b)}");
+            Console.ResetColor();
+        }
+
         static void Main(String[] args)
         {
             Console.Clear();
-            Func<int, int, int> tinhtoan;
-            tinhtoan = (a, b) => a + b;
-            Action<int, int, Func<int, int, int>> ac;
-            ac = (a, b, callback) =>
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                System.Console.WriteLine($"a + b = {callback?.Invoke(a, b)}");
-                Console.ResetColor();
-            };
 
-            // int res = (int) tinhtoan?.Invoke(3, 4);
-            ac(3, 4, (int a, int b) => a + b);
+            // Func<int, int, int> tinhtoan;
+            // tinhtoan = (a, b) => a + b;
+            // Action<int, int, Func<int, int, int>> ac;
+            // ac = (a, b, callback) =>
+            // {
+            //     Console.ForegroundColor = ConsoleColor.Yellow;
+            //     System.Console.WriteLine($"a + b = {callback?.Invoke(a, b)}");
+            //     Console.ResetColor();
+            // };
+            // Test(3, 4, (int a, int b) => a + b);
+            int[] mang = { 1, 2, 3, 4, 5, 6, 9, 16 };
+            var res =
+                mang
+                    .Select((int x) =>
+                    {
+                        return Math.Sqrt(x);
+                    });
+            foreach (var item in res)
+            {
+                System.Console.WriteLine (item);
+            }
+            System.Console.WriteLine("Old number: ");
+            mang
+                .ToList()
+                .ForEach((int e) =>
+                {
+                    if (e % 2 == 0) System.Console.WriteLine(e);
+                });
+            System.Console.WriteLine("Cac so chia het cho 4: ");
+            var cacSoChiaHetCho4 =
+                mang
+                    .Where((x) =>
+                    {
+                        return x % 4 == 0;
+                    });
+            foreach (var item in cacSoChiaHetCho4)
+            {
+                System.Console.WriteLine (item);
+            }
         }
     }
 }
